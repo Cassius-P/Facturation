cca0093b0caa3ec416750973e71743feb3081469
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-12-07 18:15:36 +0100

    Push project

---
+++ Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Facturations.Shared;
+
+namespace Facturations.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+
+            var data = Enumerable.Range(1, 30).Select(index => new WeatherForecast
+            {
+                DateE = RandomDayFunc(),
+                MontantR = rng.Next(1500, 5500),
+                Client = Summaries[rng.Next(Summaries.Length)],
+                NumFacture = index
+            }).ToArray();
+
+            return data;
+        }
+
+        DateTime RandomDayFunc()
+        {
+            DateTime start = new DateTime(2019, 1, 1);
+            Random gen = new Random();
+            int range = ((TimeSpan)(DateTime.Today - start)).Days;
+            return start.AddDays(gen.Next(range));
+        }
+    }
+}
---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facturations.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime DateE { get; set; }
+
+        public DateTime DateR => (DateTime)(DateE.AddDays(15));
+
+        public int NumFacture { get; set; }
+
+        public int MontantR { get; set; }
+
+        public string Client { get; set; }
+
+        public int MontantD => 32 + (int)(MontantR / 0.5556);
+    }
+}
